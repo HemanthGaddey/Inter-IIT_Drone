986a2a08465e6f0bd4ca826ff94628d79ada1d8f
Shubham <sbdaule5@gmail.com>
2023-01-28 18:35:09 +0530

    Fix some bugs

--- ArUco.py
+++ ArUco.py
@@ -14,86 +14,99 @@ class Camera:
         self.Marker_size    =   2.7 #centimeters
         self.Marker_dict    =   aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
         self.Para_markers   =   aruco.DetectorParameters()
-        
-        self.capture = 0
-        
-        (self.Xo,self.Yo) = (0,0)
-        (self.x,self.y) = (0,0)
-        self.coords_available = False
-        self.pid = False
 
-  
-    def get_marker_data(self, dist, coords):
-    
-        self.capture = cv2.VideoCapture(cv2.CAP_ANY,cv2.CAP_V4L2) #NEVER CHANGE THIS ON LINUX
+        self.capture = cv2.VideoCapture(0, cv2.CAP_V4L2)
         self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 10000)
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 10000)
         self.capture.set(cv2.CAP_PROP_FPS, 60)
         self.capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
 
+        (self.Xo,self.Yo) = (0,0)
+        (self.x,self.y) = (0,0)
+        self.coords_available = False
+        self.pid = False
+
+
+    def get_marker_data(self, dist, coords, assign):
+
+        #  print("DEBUG::Inside Function")
+
         while True:
+                #  print("DEBUG::Inside loop")
                 ret,frame = self.capture.read()
-                
+
                 if ret==False:
+                        print("INFO::Unable to read frame. Exiting")
                         break
                 grayimg=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 marker_corners, marker_ids, reject = aruco.detectMarkers(grayimg,self.Marker_dict, parameters=self.Para_markers)
-                
+
                 if marker_corners:
                         (x1,y1)=(int(marker_corners[0][0][-1][0]),int(marker_corners[0][0][-1][1]))
                         (x2,y2)=(int(marker_corners[0][0][1][0]),int(marker_corners[0][0][1][1]))
-                        
+
                         (self.x,self.y)=((x1+x2)//2,(y1+y2)//2)
-                        
+
                         rVec, tVec, _ = aruco.estimatePoseSingleMarkers(marker_corners, self.Marker_size, self.camMatrix, self.distCof)
                         total_markers = range(0, marker_ids.size)
                         for j in range(len(marker_ids)):
                                 for i in range(4):
                                         cv2.line(frame,(int(marker_corners[j][0][i-1][0]),int(marker_corners[j][0][i-1][1])),(int(marker_corners[j][0][i][0]),int(marker_corners[j][0][i][1])),(0,255,0),4)
-                                        
+
                                 point = cv2.drawFrameAxes(frame, self.camMatrix, self.distCof, rVec[j], tVec[j], 4, 4)
                                 distance = np.sqrt(tVec[j][0][2] ** 2 + tVec[j][0][0] ** 2 + tVec[j][0][1] ** 2)
                                 distance=185-distance
                                 distance=(distance+22)/1.09
                                 dist[0] = distance
+                                assign(dist, coords)
 
-                                
+
                                 cv2.putText(frame, f"{distance}", (70,80),  cv2.FONT_HERSHEY_PLAIN, 3, (0,0,255),2,cv2.LINE_AA)
-                                
+
                                 cv2.putText(frame,f"x:{round(rVec[j][0][0],1)} y: {round(rVec[j][0][1],1)} ",(70,150),cv2.FONT_HERSHEY_PLAIN,3,(0,255,0),2,cv2.LINE_AA)
-                                
+
                                 X=self.x-self.Xo
                                 Y=self.Yo-self.y
-                                coords[0] = int(X)
-                                coords[1] = int(Y)
+                                print(f"Inside ArUco: X : {X}")
+                                print(f"Inside ArUco: Y : {Y}")
+                                coords[0] = X
+                                coords[1] = Y
+                                assign(dist, coords)
                                 self.coords_available = True
-                                
-                                cv2.putText(frame,f"({X},{Y}),", (70,500),cv2.FONT_HERSHEY_PLAIN,3,(0, 0, 255),1,cv2.LINE_AA)
-                                
+
+                                cv2.putText(frame,f"({X},{Y}),", (70,500),cv2.FONT_HERSHEY_PLAIN,3,(0, 0, 255),2,cv2.LINE_AA)
+
                 cv2.imshow("frame",frame)
-                
+
                 key=cv2.waitKey(1)
                 if key==ord("o"):
-                	(self.Xo,self.Yo)=(self.x,self.y)
+                    (self.Xo,self.Yo)=(self.x,self.y)
                 if key==ord("x"):
-                	cv2.imwrite("frame.png",frame)
+                    cv2.imwrite("frame.png",frame)
+                # capital P will stop pid, small p will begin
                 if key==ord("p"):
-                	if(self.pid == True):
-                		self.pid = False
-                	elif(self.pid == False):
-                		self.pid == True
+                    self.pid == True
+                if key==ord("P"):
+                    self.pid = False
                 elif key==ord("c"):
                         break
-        
-        self.capture.release() 
+
+        self.capture.release()
         cv2.destroyAllWindows()
 
         exit()
 
+d = [0]
+coords = [0,0]
+def assign(p_d, p_coords):
+    global d
+    global coords
+    d = p_d
+    coords = p_coords
+
 if __name__ == "__main__":
     cam = Camera()
     print("Started..")
-    d = [0]
-    coords = [0,0]
     while True:
-        cam.get_marker_data(d,coords)
+        print("Getting Marker data")
+        cam.get_marker_data(d, coords, assign)
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import ArUco
 import threading
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from simple_pid import PID
+from simple_pid.PID import PID
 import pyMultiWii
 import time
 import sys, select, termios, tty
 import socket
 
+#------------ Drone --------------
+#  myDrone = pyMultiWii.pyMultiWii("192.168.4.1", 23)
 
 #-------------- PID --------------
 Kp = 0.5
@@ -31,33 +33,42 @@ roll_pid_op = (False,2500)
 
 def pid_function(cam):
     global coords
+    while True:
+        if(cam.coords_available):
+            print("Coords received from camera")
+            x_error = coords[0]/20.48 #system Res   #[-100,+100]
+            y_error = coords[1]/15.36
+            print(coords)
+            if(x_error>=0):
+                roll_pid_op = (True, 2600)
+                print(f"Roll 1100")
+            else:
+                roll_pid_op = (True, 2400)
+                print(f"Roll 900")
 
-    if(cam.coords_available):
-        pitch_pid_op[0] = True
-        roll_pid_op[0] = True
+            if(y_error>=0):
+                pitch_pid_op = (True, 3600)
+                print(f"Pitch 1100")
+            else:
+                pitch_pid_op = (True, 3400)
+                print(f"Pitch 900")
 
-        x_error = coords[0]/20.48 #system Res	#[-100,+100]
-        y_error = coords[1]/15.36
-        if(x_error>=0):
-        	roll_pid_op[1] = 2600
-        else:
-        	roll_pid_op[1] = 2400
-        
-        if(y_error>=0):
-        	pitch_pid_op[1] = 3600
-        else:
-        	pitch_pid_op[1] = 3400
-			
-        #pitch_pid_op[1] = pid(y_error)*15 + 1500 + 1000
-        #roll_pid_op[1] = pid(x_error)*15 + 1500 + 2000
-        
-        cam.coords_available = False
+            #pitch_pid_op[1] = pid(y_error)*15 + 1500 + 1000
+            #roll_pid_op[1] = pid(x_error)*15 + 1500 + 2000
+
+            cam.coords_available = False
 
 
 #-------------- Aruco OpenCV Object --------------
 cam = ArUco.Camera()
 d = [0]
 coords = [0,0]
+def assign(p_d, p_coords):
+    global d
+    global coords
+    d = p_d
+    coords = p_coords
+
 #-------------- MATPLOTLIB --------------
 x_len = 200         # Number of points to display
 y_range = [0, 184]  # Range of possible Y values to display
@@ -78,9 +89,9 @@ line, = ax.plot(xs, ys)
 
 def animate(i, ys):
     global d
-    
+
     temp_c = d[0]
-    
+
     ys.append(temp_c) # Add y to list
     ys = ys[-x_len:] # Limit y list to set number of items
 
@@ -88,14 +99,14 @@ def animate(i, ys):
 
     return line,
 
-ani = animation.FuncAnimation(fig,
-    animate,
-    fargs=(ys,),
-    interval=5,
-    blit=True)
-    
+#  ani = animation.FuncAnimation(fig,
+#      animate,
+#      fargs=(ys,),
+#      interval=5,
+#      blit=True)
+
 #-------------- a.PY --------------
-HOST = '127.0.0.1'        
+HOST = '127.0.0.1'
 PORT = 60000
 settings = termios.tcgetattr(sys.stdin)
 
@@ -157,20 +168,20 @@ def a_py():
 
                         if key in keyboard_control.keys():
                             s.send(bytes(str(keyboard_control[key]), 'utf-8'))
-                        #if pitch_pid_op[0]:
-                        	#s.send(bytes(str(pitch_pid_op[1]),'utf-8'))
-                        	#pitch_pid_op[0] = False
-                        #if roll_pid_op[0]:
-                            #s.send(bytes(str(roll_pid_op[1]),'utf-8'))
-                            #roll_pid_op[0] = False
+                        if pitch_pid_op[0]:
+                            s.send(bytes(str(pitch_pid_op[1]),'utf-8'))
+                            pitch_pid_op[0] = False
+                        if roll_pid_op[0]:
+                            s.send(bytes(str(roll_pid_op[1]),'utf-8'))
+                            roll_pid_op[0] = False
                         elif(time.time()-ct >= 22/1000):
-                        	s.send(bytes(str(80), 'utf-8'))
-                        	ct = time.time()
+                            s.send(bytes(str(80), 'utf-8'))
+                            ct = time.time()
                         else:
-                        	s.send(bytes(str(10),'utf-8'))
-                        	s.send(bytes(str(40),'utf-8'))
-                        					
-                        
+                            s.send(bytes(str(10),'utf-8'))
+                            s.send(bytes(str(40),'utf-8'))
+
+
                 finally:
                     pass
                 termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
@@ -184,14 +195,64 @@ def print_data():
         print(d[0],pitch_pid_op[1]-1000,roll_pid_op[1]-2000)
 
 if __name__ == '__main__':
+    print(1)
+    cam_thread = threading.Thread(target=cam.get_marker_data, args=(d, coords, assign))
+    #  data_print_thread = threading.Thread(target=print_data, args=())
+    #  a_py_thread = threading.Thread(target=a_py, args=())
+    pid_thread = threading.Thread(target=pid_function, args=(cam, ))
 
-    cam_thread = threading.Thread(target=cam.get_marker_data, args=(d,coords,))
-    #data_print_thread = threading.Thread(target=print_data, args=())
-    a_py_thread = threading.Thread(target=a_py, args=())
-        
     cam_thread.start()
+    pid_thread.start()
     #data_print_thread.start()
-    a_py_thread.start()
-    
-    plt.show()
-    
+    #  a_py_thread.start()
+
+    #  plt.show()
+    #  state = 0
+    #  init_time = time.time()
+    #  ct = time.time()
+    #  ct = time.time()
+    #  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #      s.connect((HOST, PORT))
+    pid_thread.join()
+    cam_thread.join()
+    while False:
+        if state == 0:
+            print("DEBUG::Arming")
+            myDrone.arm()
+            #  s.send(bytes("70", 'utf-8'))
+            state = 5
+        elif state == 5:
+            if(time.time() - init_time > 0.5):
+                print("DEBUG::Taking off")
+                #  s.send(bytes("130",'utf-8'))
+                myDrone.take_off()
+                state = 10
+        elif state == 10:
+            if(time.time() - init_time > 1):
+                print("DEBUG::Moving")
+                #  s.send(bytes(str(2600),'utf-8'))
+                myDrone.setPitch(1100)
+                state = 20
+        elif state == 20:
+            if(time.time() - init_time > 2):
+                print("DEBUG::Moving")
+                #  s.send(bytes(str(3600),'utf-8'))
+                myDrone.setRoll(1100)
+                state = 30
+        elif state == 30:
+            if(time.time() - init_time > 3):
+                print("DEBUG::Landing")
+                #  s.send(bytes(str(140),'utf-8'))
+                myDrone.land()
+                state = 40
+        elif state == 40:
+            if(time.time() - init_time > 4):
+                print("DEBUG::Disarming")
+                #  s.send(bytes(str(170),'utf-8'))
+                myDrone.disarm()
+                state = 50
+        if(time.time() - ct >= 22/1000):
+            #  s.send(bytes(str(80), 'utf-8'))
+            myDrone.reset()
+            ct = time.time()
+
